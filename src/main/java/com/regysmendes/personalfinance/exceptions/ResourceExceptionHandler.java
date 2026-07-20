20b3fa2ab852dfa4b76cb04246e1d93ab55bc203
regys-mendes <regyseduardomendes123@gmail.com>
2026-07-20 17:30:48 -0300

    feat: add custom validation error handling with field-level messages

--- src/main/java/com/regysmendes/personalfinance/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/regysmendes/personalfinance/exceptions/ResourceExceptionHandler.java
@@ -3,6 +3,8 @@ package com.regysmendes.personalfinance.exceptions;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -22,5 +24,21 @@ public class ResourceExceptionHandler {
                         request.getRequestURI());
 
         return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e, HttpServletRequest request){
+        ValidationError err = new ValidationError();
+        err.setTimeStamp(new Date());
+        err.setStatus(HttpStatus.BAD_REQUEST.value());
+        err.setError("Validation error");
+        err.setMessage(e.getMessage());
+        err.setPath(request.getRequestURI());
+
+        for (FieldError x : e.getBindingResult().getFieldErrors()){
+            err.addError(x.getField(), x.getDefaultMessage());
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 }
