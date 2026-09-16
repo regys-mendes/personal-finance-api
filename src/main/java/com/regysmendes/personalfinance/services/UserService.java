87e211f60c06090f20cf4f721a9c2f26d00524e2
regys-mendes <regyseduardomendes123@gmail.com>
2026-09-16 18:55:37 -0300

    feat: add UserService, UserRepository and PasswordEncoder configuration

---
+++ src/main/java/com/regysmendes/personalfinance/services/UserService.java
@@ -0,0 +1,34 @@
+package com.regysmendes.personalfinance.services;
+
+import com.regysmendes.personalfinance.entities.User;
+import com.regysmendes.personalfinance.exceptions.ObjectNotFoundException;
+import com.regysmendes.personalfinance.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository repository, PasswordEncoder passwordEncoder) {
+        this.repository = repository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User findByEmail(String email){
+        Optional<User> user = repository.findByEmail(email);
+        return user.orElseThrow(() -> new ObjectNotFoundException("User not found. Email" + email));
+    }
+
+    public User insert(User dto){
+        User user = new User(dto.getId(), dto.getName(), dto.getEmail(), dto.getPassword());
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        repository.save(user);
+        return user;
+    }
+
+}
