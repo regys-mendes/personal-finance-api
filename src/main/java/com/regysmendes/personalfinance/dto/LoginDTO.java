0e4589cf321b9fa4aba9bba7a029b16dee915321
regys-mendes <regyseduardomendes123@gmail.com>
2026-09-18 18:37:41 -0300

    feat: create LoginDTO

---
+++ src/main/java/com/regysmendes/personalfinance/dto/LoginDTO.java
@@ -0,0 +1,34 @@
+package com.regysmendes.personalfinance.dto;
+
+import java.io.Serializable;
+
+public class LoginDTO implements Serializable {
+
+    private String email;
+    private String password;
+
+
+    public LoginDTO(){
+    }
+
+    public LoginDTO(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
