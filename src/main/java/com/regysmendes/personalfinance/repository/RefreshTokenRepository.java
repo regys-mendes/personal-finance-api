ad6ac0cf90b737ede7ce11bba4796f35b4687046
regys-mendes <regyseduardomendes123@gmail.com>
2026-09-23 22:13:24 -0300

    feat: create RefreshTokenRepository

---
+++ src/main/java/com/regysmendes/personalfinance/repository/RefreshTokenRepository.java
@@ -0,0 +1,14 @@
+package com.regysmendes.personalfinance.repository;
+
+import com.regysmendes.personalfinance.entities.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
+   Optional<RefreshToken> findByTokenValue(String tokenValue);
+
+}
